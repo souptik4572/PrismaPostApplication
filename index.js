a54b635e3c3b3777cf2c43b4b5431491ad4c6d5a
souptik4572 <souptiksarkar4572@gmail.com>
2021-12-12 10:08:25 +0530

    added cors for cros origin server browser request support

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const userRoutes = require('./routes/users');
 const postRoutes = require('./routes/posts');
 
 const app = express();
+
+app.use(cors);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
